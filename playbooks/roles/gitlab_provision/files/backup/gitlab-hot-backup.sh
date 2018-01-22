6c7d60719450a11b31b60b4e6fda2498b81cc69f
amin <a.fatallah.ext@comutitres.fr>
2018-01-22 18:06:15 +0100

    cron gitlab hot backup every day @2

--- playbooks/roles/gitlab_provision/files/backup/gitlab-hot-backup.sh
+++ playbooks/roles/gitlab_provision/files/backup/gitlab-hot-backup.sh
@@ -1,1 +1,15 @@
-docker exec -it gitlab-ci bundle exec rake gitlab:backup:create
+# Delete the expired backup files older than 7 days
+find /volumes/gitlab/data/backups -mtime +7 -exec rm {} \;
+
+# Run hot backup on gitlab-ci container
+sudo docker exec -i gitlab-ci bundle exec rake gitlab:backup:create
+
+# Get the last backup file
+LAST_BACKUP_FILE=$(sudo ls -1t /volumes/gitlab/data/backups/* | head -1)
+
+# Add write permission to the last modified backup file
+sudo chmod 755 $LAST_BACKUP_FILE
+
+# Transfer backup file to the archive machines
+scp -p $LAST_BACKUP_FILE amin@vm1.dev-comutitres.fr:/backups
+scp -p $LAST_BACKUP_FILE amin@vm2.dev-comutitres.fr:/backups
