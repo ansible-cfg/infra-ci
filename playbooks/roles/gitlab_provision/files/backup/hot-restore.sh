af3240ac337996c2725eb0745dda584d80202aa1
amin <a.fatallah.ext@comutitres.fr>
2018-01-25 09:56:43 +0100

    fix restore backups

--- playbooks/roles/gitlab_provision/files/backup/hot-restore.sh
+++ playbooks/roles/gitlab_provision/files/backup/hot-restore.sh
@@ -1,5 +1,8 @@
 # Download the backup file to restore
-scp -p robocop@vm1.dev-comutitres.fr:/backups/$1_gitlab_backup.tar robocop@vm4.dev-comutitres.fr:/volumes/gitlab/data/backups
+scp -p robocop@vm1.dev-comutitres.fr:/backups/$1_gitlab_backup.tar /home/robocop
+
+# Copy the downloaded file in backups directory
+sudo cp /home/$1_gitlab_backup.tar /volumes/gitlab/data/backups
 
 # Run hot restore on gitlab-ci container within backup identifier
 sudo docker exec -it gitlab-ci bundle exec rake gitlab:backup:restore BACKUP=$1
