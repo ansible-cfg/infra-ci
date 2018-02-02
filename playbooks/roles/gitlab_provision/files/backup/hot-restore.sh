f679e1cce963105a58d99b770367e565237b3d5c
amin <a.fatallah.ext@comutitres.fr>
2018-02-02 09:35:44 +0100

    Clear gitlab cache after restore

--- playbooks/roles/gitlab_provision/files/backup/hot-restore.sh
+++ playbooks/roles/gitlab_provision/files/backup/hot-restore.sh
@@ -2,13 +2,17 @@
 scp -p robocop@vm1.dev-comutitres.fr:/backups/$1_gitlab_backup.tar /home/robocop
 
 # Copy the downloaded file in backups directory
-sudo cp /home/$1_gitlab_backup.tar /volumes/gitlab/data/backups
+sudo cp /home/robocop/$1_gitlab_backup.tar /volumes/gitlab/data/backups
 
 # Run hot restore on gitlab-ci container within backup identifier
 sudo docker exec -it gitlab-ci bundle exec rake gitlab:backup:restore BACKUP=$1
 
 # Copy certs
 /bin/sh /backup/scripts/copy-certs.sh
+
+# Clear gitlab cache
+sudo docker exec -it gitlab-ci chown -R git:git /home/git/
+sudo docker exec -it gitlab-ci bundle exec rake cache:clear RAILS_ENV=production
 
 # /!\ Sometimes, repositories are empty after data restore. 
 # You have to clean cache on Redis container using redis-cli.
